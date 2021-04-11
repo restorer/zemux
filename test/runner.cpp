14cf124229cd3fe5b570b2df35bc8b58c13f2832
Viachaslau Tratsiak <viachaslau@fastmail.com>
2021-04-11 16:48:39 +0300

    Bump copyrights

--- test/runner.cpp
+++ test/runner.cpp
@@ -1,7 +1,7 @@
 /*
  * MIT License (http://www.opensource.org/licenses/mit-license.php)
  *
- * Copyright (c) 2020, Viachaslau Tratsiak (aka restorer)
+ * Copyright (c) 2021, Viachaslau Tratsiak (aka restorer)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
